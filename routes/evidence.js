c6516fc9f300c95e6539aba80052f3c7b758d892
igaln <igal@madwellnyc.com>
2014-05-18 12:58:43 -0400

    evidence event relation fix

--- routes/evidence.js
+++ routes/evidence.js
@@ -4,12 +4,12 @@
     View: views/evidence_*.ejs
 */
 
-var express         = require('express');
-var router          = express.Router();
-var mongoose        = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
 
-var Evidence        = mongoose.model( 'Evidence' );
-var Event           = mongoose.model('Event');
+var Evidence = mongoose.model('Evidence');
+var Event = mongoose.model('Event');
 
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
@@ -26,8 +26,11 @@ var multipartMiddleware = multipart();
 // GET /evidences/1
 router.get('/:id', function(req, res) {
 
-  Evidence.findById(req.params.id, function (err, evidence) {
-    res.render('evidence_show', { title: 'Tutanak '+ req.params.id, evidence: evidence });
+  Evidence.findById(req.params.id, function(err, evidence) {
+    res.render('evidence_show', {
+      title: 'Tutanak ' + req.params.id,
+      evidence: evidence
+    });
   });
 
 });
@@ -40,22 +43,25 @@ router.get('/', function(req, res) {
   // this might not be the best practice moving forward,
   // TODO: best to have a interface to match events with evidences.
 
-  var options = { 
-    limit:1,
-    sort:{
-        date_added: 1 //Sort by Date Added ASC
+  var options = {
+    limit: 1,
+    sort: {
+      date_added: 1 //Sort by Date Added ASC
     }
   }
   var filter = {};
 
-  Event.find(filter,'name',options,function(err,events) {
+  Event.find(filter, 'name', options, function(err, events) {
 
-       //@igaln: removed the Evidence query here, @arikan, not sure why it was being used.     
-       // we have to have at least 1 event to add evidences to
-       if(events.length > 0) 
-         res.render('evidence_new', {title: 'Yeni tutanak kanıtı gir',current_event:events[0]});
-       else 
-         res.send("Sorry, nothing is happening in the world.")
+    //@igaln: removed the Evidence query here, @arikan, not sure why it was being used.     
+    // we have to have at least 1 event to add evidences to
+    if (events.length > 0)
+      res.render('evidence_new', {
+        title: 'Yeni tutanak kanıtı gir',
+        current_event: events[0]
+      });
+    else
+      res.send("Sorry, nothing is happening in the world.")
   });
 
 });
@@ -64,78 +70,93 @@ router.get('/', function(req, res) {
 // GET /evidences/1/edit
 router.get('/:id/edit', function(req, res) {
 
-  Evidence.findById(req.params.id, function (err, evidence) {
-    res.render('evidence_edit', { title: 'Tutanak '+ req.params.id + ' düzenle', evidence: evidence });
+  Evidence.findById(req.params.id, function(err, evidence) {
+    res.render('evidence_edit', {
+      title: 'Tutanak ' + req.params.id + ' düzenle',
+      evidence: evidence
+    });
   });
 
 });
 
 
-router.post('/', multipartMiddleware,function(req, res) {
+router.post('/', multipartMiddleware, function(req, res) {
 
 
   // post params
-   var city = req.body.city;
-   var district = req.body.district;
-   var evidenceno = req.body.no;
-   var current_event = req.body.event_id;
+  var city = req.body.city;
+  var district = req.body.district;
+  var evidenceno = req.body.no;
+  var current_event = req.body.event_id;
 
   //begin s3 file upload
   var fs = require('fs');
   var AWS = require('aws-sdk');
 
   AWS.config.loadFromPath('./config/s3creds.json');
-  AWS.config.update({region: 'us-east-1'});
-  var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  AWS.config.update({
+    region: 'us-east-1'
+  });
+  var s3 = new AWS.S3({
+    apiVersion: '2006-03-01'
+  });
 
-      // Lists your s3 buckets, using this for debug 
-      // s3.listBuckets(function(err, data) {
-      //     for (var index in data.Buckets) {
-      //       var bucket = data.Buckets[index];
-      //       console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
-      //     }
-      // });
+  // Lists your s3 buckets, using this for debug 
+  // s3.listBuckets(function(err, data) {
+  //     for (var index in data.Buckets) {
+  //       var bucket = data.Buckets[index];
+  //       console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+  //     }
+  // });
 
 
-      // create filename string from date 
-      //TODO: make this a proper CITY + DISTRICT + NO and extension
-      var d = Date.now();
-      var filetosave  = d.toString() + "_" + req.files.image.name;
-   
-    fs.readFile(req.files.image.path, function (err, data) {  
-      
-       var params = {
-            Bucket: "journosweb", 
-            Key:  "sandik/images/" + filetosave, 
-            Body: data,
-            ContentType: 'application/image'
+  // create filename string from date 
+  //TODO: make this a proper CITY + DISTRICT + NO and extension
+  var d = Date.now();
+  var filetosave = d.toString() + "_" + req.files.image.name;
+
+  fs.readFile(req.files.image.path, function(err, data) {
+
+    var params = {
+      Bucket: "journosweb",
+      Key: "sandik/images/" + filetosave,
+      Body: data,
+      ContentType: 'application/image'
+    };
+
+    s3.putObject(params, function(error, data) {
+
+      if (error) {
+        console.log(error);
+        callback(error, null);
+      } else {
+
+        var doc = {
+          city: city,
+          district: district,
+          no: evidenceno,
+          img: filetosave
         };
+        new Evidence(doc).save(function(err, evidence, count) {
+          if (err) return handleError(err);
 
-       s3.putObject(params, function(error, data) {
-           
-            if (error) {
-                console.log(error);
-                callback(error, null);
-            } else {
-               
-                    var doc = {city:city,district:district,no:evidenceno,img:filetosave};
-                    new Evidence(doc).save( function( err, evidence, count ){
-                          if (err) return handleError(err); 
-                         
-                          Event.findById(current_event, function ( err, event_result ){
+          //TODO: Make the data association in the model
+          Event.findById(current_event, function(err, event_result) {
 
-                                if (err) return handleError(err);
+            if (err) return handleError(err);
 
-                                event_result.evidences.push(evidence);
-                                res.redirect( '/evidence' );
-                          });
+            event_result.evidences.push(evidence);
+            event_result.save(function(err, result) {
+              if (err) return handleError(err);
 
-                    });
-            }
+              res.redirect('/evidence');
+            });
+          });
         });
-
+      }
     });
 
+  });
 });
 
 // update
@@ -144,13 +165,17 @@ router.put('/', function(req, res) {
 
   var evidence_id = reg.body.evidence_id;
 
-  var update_query = {updated_at:Date.now()}
-  var update_filter = {_id:evidence_id}
+  var update_query = {
+    updated_at: Date.now()
+  }
+  var update_filter = {
+    _id: evidence_id
+  }
 
-  Evidence.findById(update_filter,update_query, function ( err, evidence_result ){
+  Evidence.findById(update_filter, update_query, function(err, evidence_result) {
 
-            if (err) return handleError(err);
-            res.send(evidence_result);
+    if (err) return handleError(err);
+    res.send(evidence_result);
   });
 });
 
@@ -160,9 +185,9 @@ router.delete('/', function(req, res) {
 
   var evidence_id = reg.body.evidence_id;
 
-  Event.fineOneAndRemove(evidence_id, function ( err, evidence_result ){
-          if (err) return handleError(err);
-            res.send(evidence_result);
+  Event.fineOneAndRemove(evidence_id, function(err, evidence_result) {
+    if (err) return handleError(err);
+    res.send(evidence_result);
   });
 
 });
