497b3ba9f7a7bfaf8b695690b860129c072d58cb
igaln <igal@madwellnyc.com>
2014-05-24 21:27:22 -0400

    reading

--- models/reading.js
+++ models/reading.js
@@ -2,16 +2,23 @@ var mongoose = require('mongoose')
    ,Schema = mongoose.Schema
    ,ObjectId = Schema.ObjectId;
 
+
 var Reading = new Schema({
-  evidence:                   {id: ObjectId},
-  kayitli_secmen:             Number,
-  oy_kullanan_secmen:         Number,
-  kanunen_oy_kullanan_secmen: Number,
-  toplam_oy_kullanan_secmen:  Number,
-  gecerli_oy:                 Number,
-  itirazli_gecerli_oy:        Number,
-  toplam_gecerli_oy:          Number,
-  gecersiz_oy:                Number,
+  evidence:                   { type : Schema.ObjectId, ref : 'Evidence' },
+  
+  kayitli_secmen:               Number,
+  oy_kullanan_secmen:           Number,
+  kanunen_oy_kullanan_secmen:   Number,
+  toplam_oy_kullanan_secmen:    Number,
+  sandiktan_cikan_zarf_sayisi:  Number,
+  gecerli_zarf_sayisi:          Number,
+  itirazsiz_gecerli_oy:         Number,
+  itirazli_gecerli_oy:          Number,
+  gecerli_oy:                   Number,
+  gecersiz_oy:                  Number,
+  toplam_gecerli_oy:            Number,
+ 
+
   results:                    [{
                                 id: ObjectId,       // Candidate.id
                                 party: String,
