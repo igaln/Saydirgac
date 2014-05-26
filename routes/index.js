958e4091ae9ce7b7582709600412765ad16a1cb7
Engin Erdogan <erdogan@gmail.com>
2014-05-25 23:46:42 -0400

    Basic layout and styles applied, home page layout changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Sayman' });
+  res.render('index', { title: 'Saydıraç' });
 });
 
 module.exports = router;
