bf19fe6d1326f6d03aef52fe84692de987c6f2c3
igaln <igal@madwellnyc.com>
2014-06-04 04:45:11 -0400

    unread

--- routes/progress.js
+++ routes/progress.js
@@ -33,13 +33,41 @@ router.get('/dashboard/live', function(req, res) {
   var data = {};
 
   Progress.find({}).populate('reading').exec(function(err, progresses){
-
       res.render('progress_dashboard', {
         title: 'Durum',
         // data: data,
         progresses:progresses
       });
     
+  });
+});
+
+// index
+// GET /progress/dashboard/
+router.get('/dashboard/unread/:format?', function(req, res) {
+
+  var results = [];
+
+  Progress
+        .find({reading_count:0}).exec(function(err, progresses){
+           if (err) return handleError(err);
+
+        progresses.forEach(function(progress) {
+              if(progress.type == "Box") {
+                results.push(progress);
+              }
+
+        });
+          if (req.params.format) {
+              res.json(results);
+          }else{
+            res.render('progress_index', {
+              title: 'Durum',
+              progresses: results
+            });
+          }
+
+      
   });
 });
 
