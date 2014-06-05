d6dd8e75545d6c5f53ad7e44b473bf1017b7fb56
igaln <igal@madwellnyc.com>
2014-06-05 01:50:30 -0400

    hotfix

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use(function(req, res, next) {
 
 // add this to run POPULATE SCRIPTS at different env  || process.env.env === "development"  || 
 
-if(process.env.env === 'local' || process.env.env === "production")
+if(process.env.env === 'local')
   mongoose.connect(config.mongoURI);
 else
   mongoose.connect(process.env.MONGOHQ_URL);
